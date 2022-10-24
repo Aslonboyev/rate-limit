d114b479699b226c8081d849b4b7c5b63c01a07e
temurofficial07 <temuraslonboyev@gmail.com>
2022-10-24 12:47:29 +0500

    create

---
+++ DemoRateLimitApp/Configurations/RateLimitingConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using AspNetCoreRateLimit;
+
+namespace DemoRateLimitApp.Configurations
+{
+    public static class RateLimitingConfiguration
+    {
+        public static void ConfigureRateLimiting(this IServiceCollection services)
+        {
+            services.Configure<IpRateLimitOptions>(option =>
+            {
+                option.EnableEndpointRateLimiting = true;
+                option.StackBlockedRequests = false;
+                option.HttpStatusCode = 429;
+                option.RealIpHeader = "X-Real-IP";
+                option.ClientIdHeader = "X-ClientId";
+                option.GeneralRules = new List<RateLimitRule> {
+                    new RateLimitRule
+                    {
+                        Endpoint = "*",
+                        Period = "1s",
+                        Limit = 2,
+                    },
+                    new RateLimitRule()
+                    {
+                        Endpoint="*",
+                        Period="1m",
+                        Limit=10,
+                    },
+                    new RateLimitRule()
+                    {
+                        Endpoint="*",
+                        Period="1h",
+                        Limit=30,
+                    }
+                };
+            });
+
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+
+            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
+            services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
+
+            services.AddInMemoryRateLimiting();
+        }
+    }
+}
---
+++ DemoRateLimitApp/Program.cs
@@ -0,0 +1,23 @@
+using AspNetCoreRateLimit;
+using DemoRateLimitApp.Configurations;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
+builder.Services.AddHttpContextAccessor();
+builder.Services.ConfigureRateLimiting();
+
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseIpRateLimiting();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
